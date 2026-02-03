42ebd7828e2a3328935d467aac8b3ee17b072991
mechelle99 <68836945+mechelle99@users.noreply.github.com>
2026-02-03 23:10:14 +0800

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,2 +1,9 @@
-window.GAS_ENDPOINT = "__GAS_ENDPOINT__";
+// config.js
+(function () {
+  var endpoint = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vg00G9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTLK9-xBpwxtmuVAQkA/exec";
+  window.GAS_ENDPOINT = endpoint;
+  window.CONFIG = window.CONFIG || {};
+  window.CONFIG.GAS_ENDPOINT = endpoint;
+})();
+
 

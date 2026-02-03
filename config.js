e8075882560eb8446ff208a7100f0abcf598e197
mechelle99 <68836945+mechelle99@users.noreply.github.com>
2026-02-03 23:20:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-// config.js
-(function () {
-  var endpoint = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vg00G9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTLK9-xBpwxtmuVAQkA/exec";
-  window.GAS_ENDPOINT = endpoint;
-  window.CONFIG = window.CONFIG || {};
-  window.CONFIG.GAS_ENDPOINT = endpoint;
-})();
+window.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vg00G9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTLK9-xBpwx tmuVAQkA/exec";

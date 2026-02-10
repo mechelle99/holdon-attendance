bdb7ef59cfba8ba9cc751928951fff06a31bda18
mechelle99 <68836945+mechelle99@users.noreply.github.com>
2026-02-10 15:38:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-window.CONFIG = {
-  GAS_ENDPOINT: "https://script.google.com/macros/s/您的部署ID/exec",
-  API_KEY: "MY_SECRET_KEY_2026" // 🔥 新增這行，跟 GAS 屬性一致
-};
-
 // config.js
 window.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vgOOG9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTlK9-xBpwxtmuVAQkA/exec";
 

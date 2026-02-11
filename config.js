627bb06bd242fdc0499931149b9ce50219a41be8
mechelle99 <68836945+mechelle99@users.noreply.github.com>
2026-02-11 19:31:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+// config.js
 window.CONFIG = window.CONFIG || {};
 window.CONFIG.GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwPDqm4YawpB5vgOOG9qqPA51-u_vCb-bbfr-91k6EtjkN6ZDrjTlK9-xBpwxtmuVAQkA/exec";
+
 window.CONFIG.WEBHOOK_KEY = "HOLDON2026";
-window.CONFIG.LIFF_ID = "2009036245-XmZMSuuS";
 
